7ccfe8ee3d859b782d8ef69e6aca2bc7a5f9f17e
emernemly <nemlyemer@gmail.com>
2024-01-11 19:59:11 +0100

    ads

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,25 @@ export default function RootLayout({ children }) {
         <React.Fragment>
           <script
             dangerouslySetInnerHTML={{
+              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W2X6XBBC');`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1233224094300219&ev=PageView&noscript=1"
+ />`,
+            }}
+          />
+        </React.Fragment>
+
+        <React.Fragment>
+          <script
+            dangerouslySetInnerHTML={{
               __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -70,7 +89,14 @@ src="https://www.facebook.com/tr?id=1233224094300219&ev=PageView&noscript=1"
       <StoreProvide>
         {' '}
         <body className={inter.className}>
-          {' '}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-W2X6XBBC"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>{' '}
           <Suspense fallback={<Loadingcompante />}>
             <ScrollUp />
             {children}
