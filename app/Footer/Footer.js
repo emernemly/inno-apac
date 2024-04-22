471ce948c7dfb64a281abe43a150752e890b2abb
emernemly <nemlyemer@gmail.com>
2024-04-22 16:40:25 +0100

    fix link

--- app/Footer/Footer.js
+++ app/Footer/Footer.js
@@ -116,6 +116,16 @@ const Footer = () => {
                 Privacy Policy
               </Link>
             </li>
+            <li className="mb-[14px] hover:text-[#e6202d] cursor-pointer">
+              {'>'}{' '}
+              <Link
+                className=" ml-2"
+                href="https://beian.miit.gov.cn/"
+                target="_blank"
+              >
+                Services
+              </Link>
+            </li>
           </ul>
         </div>
         <div className=" flex md:justify-center flex-col text-white">
